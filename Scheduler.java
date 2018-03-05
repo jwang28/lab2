01968968704ffc77c82e4d5748a0bce0546eff5c
Jennifer Wang <Jwang28@gmail.com>
2018-03-05 00:55:50 -0500

    Add new changes

--- Scheduler.java
+++ Scheduler.java
@@ -1,15 +1,15 @@
 //need max finishing time variable
 
 import java.io.*;
-import java.uti.*;
+import java.util.*;
 
 public class Scheduler {
 
 	public static void main (String [] args){
-		Arraylist<Process> procArr1 = new Arraylist<Process>();
-		Arraylist<Process> procArr2 = new Arraylist<Process>();
-		Arraylist<Process> procArr3 = new Arraylist<Process>();
-		Arraylist<Process> procArr4 = new Arraylist<Process>();
+		ArrayList<Process> procArr1 = new ArrayList<Process>();
+		ArrayList<Process> procArr2 = new ArrayList<Process>();
+		ArrayList<Process> procArr3 = new ArrayList<Process>();
+		ArrayList<Process> procArr4 = new ArrayList<Process>();
 		File f;
 		boolean verbose = false;
 
@@ -20,19 +20,25 @@ public class Scheduler {
 		else{
 			f = new File (args[0]);
 		}
-		Scanner input = new Scanner(f);
-		int numProcesses = input.nextInt();
-		int A,B,C,io;
-		for (int i = 0; i < numProcesses; i++){
-			A = input.nextInt();
-			B = input.nextInt();
-			C = input.nextInt();
-			io = input.nextInt();
-			procArr1.add(new Process(A,B,C,io));
-			procArr2.add(new Process(A,B,C,io));
-			procArr3.add(new Process(A,B,C,io));
-			procArr4.add(new Process(A,B,C,io));
+		try{
+			Scanner input = new Scanner(f);
+			int numProcesses = input.nextInt();
+			int A,B,C,io;
+			for (int i = 0; i < numProcesses; i++){
+				A = input.nextInt();
+				B = input.nextInt();
+				C = input.nextInt();
+				io = input.nextInt();
+				procArr1.add(new Process(A,B,C,io));
+				procArr2.add(new Process(A,B,C,io));
+				procArr3.add(new Process(A,B,C,io));
+				procArr4.add(new Process(A,B,C,io));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(0);
 		}
+		
 	}
 
 }
